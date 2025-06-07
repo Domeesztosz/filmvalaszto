35dbf259a27c29ca9b7a9c2bdae926af16e96e0d
Domeesztosz <p.doma1211@gmail.com>
2025-06-07 11:18:51 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -46,11 +46,3 @@ function pickMovie() {
   const chosenMovie = movies[randomIndex];
   document.getElementById("result").textContent = `Filmajánló: 🍿 ${chosenMovie}`;
 }
-document.addEventListener("mousemove", (e) => {
-  const x = e.clientX;
-  const y = e.clientY;
-
-  document.body.style.background = `
-    radial-gradient(circle 80px at ${x}px ${y}px, rgba(0,120,255,0.4), rgba(0,30,60,1) 100px)
-  `;
-});
